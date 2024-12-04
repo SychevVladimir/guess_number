4d5383d0cdc870cbdbd0dd9990566ae995cfe16c
Vladimir Sychev <vladimir.sy4ev@yandex.ru>
2024-12-04 19:47:59 +0300

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,32 @@
+from random import randint
+
+
+WELCOME = 'Угадайте число от 1 до 100!'
+
+
+number = randint(1, 100)
+print(WELCOME)
+
+
+
+def test():
+    while True:
+        guess = int(input('Введите чило: '))
+
+        if guess < number:
+            print('Ваше число меньше')
+        
+
+        if guess > number:
+            print('Ваше число больше')
+        
+
+        if guess == number:        
+            print('Вы угодали число')
+        
+            break
+
+
+test()
+
+print('Отлично! Вы угодали число')
